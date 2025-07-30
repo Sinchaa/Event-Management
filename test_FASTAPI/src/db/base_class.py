b1e152482281edc3aaf82660b5bf1c9f71e040cf
Sinchana <43563661+Sinchaa@users.noreply.github.com>
2025-07-30 23:16:49 +0530

    Delete test_FASTAPI directory

--- test_FASTAPI/src/db/base_class.py
+++
@@ -1,6 +0,0 @@
-from typing import Any
-from sqlalchemy.ext.declarative import declared_attr,declarative_base
-from sqlalchemy.orm import as_declarative
-from sqlalchemy import create_engine,MetaData
-
-Base = declarative_base(metadata=MetaData(schema='public'))
--- test_FASTAPI/src/modules/register_event/validate_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class CreateAttendeeRequest(BaseModel):
-    name: str
-    email: str
-
-    class Config:
-        orm_mode = True
